06d36972a787862ef03b150f97fb259b89a49cab
Jay Madden <jaymaddencox@gmail.com>
2023-03-11 17:52:32 -0600

    Add netlify env variables for s3

--- src/domain/storage.ts
+++ src/domain/storage.ts
@@ -1,7 +1,17 @@
 import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
 
 export async function getDataFromBucket<T>(file: string): Promise<T | null> {
-  const client = new S3Client({ region: 'us-east-1' })
+
+  let client: S3Client;
+  if (process.env.PROD) {
+    client = new S3Client({ region: 'us-east-1', credentials: {
+        accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID || '',
+        secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY || ''
+      } })
+  }
+  else {
+    client = new S3Client({ region: 'us-east-1'});
+  }
 
   const command = new GetObjectCommand({
     Bucket: 'gm-dashboard-bucket',
