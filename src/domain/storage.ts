8926d056ae4fbeaa5858ca87c9f0ddfdb37bc464
Jay Madden <jaymaddencox@gmail.com>
2023-03-11 18:11:45 -0600

    More netlify env work

--- src/domain/storage.ts
+++ src/domain/storage.ts
@@ -3,7 +3,8 @@ import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
 export async function getDataFromBucket<T>(file: string): Promise<T | null> {
 
   let client: S3Client;
-  if (process.env.PROD) {
+  console.log(process.env.PROD)
+  if (process.env.PROD === 'true') {
     client = new S3Client({ region: 'us-east-1', credentials: {
         accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID || '',
         secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY || ''
